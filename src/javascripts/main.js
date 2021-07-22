1d6d9e3cc5f0a9ce64d9d856800fd96bcf17820d
湯浅 信彦 <2534721+yuasys@users.noreply.github.com>
2021-07-23 04:24:25 +0900

    Install Typescript

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,10 +5,13 @@ import '../stylesheets/main.scss';
 import Vue from 'vue';
 import VueApp from './VueApp.vue';
 
+import add from './add.ts';
+
+console.log(add(3, 9));
+console.log('This is index.js');
+my();
+
 new Vue({
   el: '#vue-root',
   render: (h) => h(VueApp),
 });
-
-console.log('This is index.js');
-my();
